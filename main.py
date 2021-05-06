9a7699ae8207fbb8e4558098739dea0dbe43db5b
deypritam8370 <deypritam8370@gmail.com>
2021-05-07 00:45:39 +0530

    PRITAM JARVIS BASIC FROM 3 TO 10

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 import pyttsx3
+import wikipedia
+
+from TakeCommand import TakeCommand
+from WishMe import WishMe
+from SendEmail import SendEmail
+
 engine = pyttsx3.init()
-engine.say("HELLO, MY NAME IS JARVIS")
-engine.runAndWait()
+import datetime
+
+
+if __name__ == '__main__':
+
+    WishMe()
+
+    while True:
+        query = TakeCommand().lower()
+
+        if 'time' in query:
+            datetime.time()
+        elif 'date' in query:
+            datetime.date()
+        elif 'wikipedia' in query:
+            pyttsx3.speak("Searching...")
+            query = query.replace("Wikipedia", "")
+            result = wikipedia.summary(query, sentences = 3)
+            pyttsx3.speak("According to Wikipedia")
+            print(result)
+            pyttsx3.speak(result)
+        elif 'send email' in query:
+            try:
+                pyttsx3.speak("What should I say?")
+                content = TakeCommand()
+                pyttsx3.speak("Who is the receiver?")
+                receiver = input("Enter Receiver Email :")
+                to = receiver
+                SendEmail(to, content)
+                pyttsx3.speak(content)
+                pyttsx3.speak("Email has been sent")
+
+            except Exception as e:
+                print(e)
+                pyttsx3.speak("Unable to send")
